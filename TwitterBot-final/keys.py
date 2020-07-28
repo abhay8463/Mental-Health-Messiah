0d2d747f1ea8e31e3430b236262279d822117be0
JayJhaveri1906 <53606587+JayJhaveri1906@users.noreply.github.com>
2020-07-28 18:47:47 +0530

    Update keys.py

--- TwitterBot-final/keys.py
+++ TwitterBot-final/keys.py
@@ -2,18 +2,20 @@
 # keys.py
 # visit http://dev.twitter.com to create an application and get your keys
 keys = dict(
-    consumer_key='Y1ALXyp3DDD0PTm86gm7lfZV9',
-    consumer_secret='fSyIalMTvZtSPdDxJEdecUH8pvGGWJ7Qrz1GUAWxdekV6ZeDFX',
-    access_token='1278660797707644930-GpX24ESBZx2EyqpFG5tua9k1gcIevv',
-    access_token_secret='PwOzhsFDy4aIeO11HER07WoB1YEvOLOH8ekck2njjDK3K',
-    ibm_key="5c9kfs4FdOlpjyYLovMapIIUPJpLLxagZIHdotDG1ITx",
-    ibm_url="https://api.eu-gb.tone-analyzer.watson.cloud.ibm.com/instances/5876329a-7eb5-4f0a-b859-20213cc78dd1",
+    consumer_key='YOUR KEY HERE',
+    consumer_secret='YOUR KEY HERE',
+    access_token='YOUR KEY HERE',
+    access_token_secret='YOUR KEY HERE',
+    ibm_key="YOUR KEY HERE",
+    ibm_url="YOUR KEY HERE",
 )
 
 reddit_keys = dict(
-    C_id="UpR5eeMb9oKQug",
-    C_secret="Jg0tchfzNEuMj4_wWhpHHi1C880",
-    User_agent="my user agent",
-    Uname="Jay_Jhaveri1906",
-    Password="Jaybittu1",
+    C_id="YOUR KEY HERE",
+    C_secret="YOUR KEY HERE",
+    User_agent="YOUR KEY HERE",
+    Uname="YOUR KEY HERE",
+    Password="YOUR KEY HERE",
 )
+
+# note: write your keys inside the quotes.
